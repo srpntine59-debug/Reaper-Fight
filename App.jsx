05a8656707b5af8186cf3c188408ba14c737aeda
srpntine59-debug <srpntine59@gmail.com>
2026-06-16 21:00:12 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div style={{
+      width: "100vw",
+      height: "100vh",
+      background: "black",
+      color: "white",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "monospace"
+    }}>
+      Game is running
+    </div>
+  );
+}
